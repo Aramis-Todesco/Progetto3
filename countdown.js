e3e747ae3892b462766643dd31007eb05dad9c41
Aramis Todesco <todesco.aramis@gmail.com>
2025-08-03 17:14:43 +0200

    aggiornamento countdown

--- countdown.js
+++ countdown.js
@@ -1,47 +1,79 @@
-export default class {
-    #scadenza;
-    #ms;
-    #htmlElem;
-    #interval;
+export default class Countdown {
+  #scadenza;
+  #ms;
+  #htmlElem;
+  #interval;
 
-    constructor(d, m12, y, h=0, m=0, s=0) {
-        this.#scadenza = new Date(y, m12-1, d, h, m, s);
-        this.#ms = {
-            inizio: this.#scadenza.getTime(),
-            in_giorno: 24 * 60 * 60 * 1000,
-            in_ora: 60 * 60 * 1000,
-            in_minuto: 60 * 1000
-        };
-        this.#htmlElem = {
-            giorni: document.querySelector("#giorni"),
-            ore: document.querySelector("#ore"),
-            minuti: document.querySelector("#minuti"),
-            secondi: document.querySelector("#secondi")
-        };
-        this.#aggiornaCountdown();
-        this.#interval = setInterval(this.#aggiornaCountdown.bind(this), 1000);
+  constructor(d, m12, y, h = 0, m = 0, s = 0) {
+    this.#scadenza = new Date(y, m12 - 1, d, h, m, s);
+    this.#ms = {
+      inizio: this.#scadenza.getTime(),
+      in_giorno: 24 * 60 * 60 * 1000,
+      in_ora: 60 * 60 * 1000,
+      in_minuto: 60 * 1000,
+    };
+    // Seleziona gli elementi di visualizzazione all'interno della sezione #time
+    this.#htmlElem = {
+      giorni: document.querySelector("#giorni"),
+      ore: document.querySelector("#ore"),
+      minuti: document.querySelector("#minuti"),
+      secondi: document.querySelector("#secondi"),
+    };
+    this.#aggiornaCountdown();
+    this.#interval = setInterval(this.#aggiornaCountdown.bind(this), 1000);
+  }
+
+  #aggiornaCountdown() {
+    let ms_correnti = new Date().getTime(),
+      ms_differenza = this.#ms.inizio - ms_correnti,
+      time = {
+        giorni: ~~(ms_differenza / this.#ms.in_giorno),
+        ore: ~~((ms_differenza % this.#ms.in_giorno) / this.#ms.in_ora),
+        minuti: ~~((ms_differenza % this.#ms.in_ora) / this.#ms.in_minuto),
+        secondi: ~~((ms_differenza % this.#ms.in_minuto) / 1000),
+      };
+
+    if (ms_differenza <= 0) {
+      // Ferma il countdown quando scade
+      return this.stopCountdown();
     }
 
-    #aggiornaCountdown() {
-        let ms_correnti = new Date().getTime(),
-            ms_differenza = this.#ms.inizio - ms_correnti,
-            time = {
-                giorni: ~~(ms_differenza / this.#ms.in_giorno),
-                ore: ~~((ms_differenza % this.#ms.in_giorno) / this.#ms.in_ora),
-                minuti: ~~((ms_differenza % this.#ms.in_ora) / this.#ms.in_minuto),
-                secondi: ~~((ms_differenza % this.#ms.in_minuto) / 1000),
-            }
+    this.#aggiornaTempi(time);
+  }
 
-        if(ms_differenza <= 0) {
-            return clearInterval(this.#interval);
-        }
-
-        this.#aggiornaTempi(time);
+  #aggiornaTempi(time) {
+    for (let chiave in this.#htmlElem) {
+      this.#htmlElem[chiave].textContent = time[chiave];
     }
-    #aggiornaTempi(time) {
-        for(let chiave in this.#htmlElem) {
-            this.#htmlElem[chiave].textContent = time[chiave];
-        }
-    }
+  }
 
+  // Metodo pubblico per fermare il countdown
+  stopCountdown() {
+    clearInterval(this.#interval);
+  }
 }
+
+// Inizializza l'applicazione dopo che il DOM è stato caricato
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("#countdownForm");
+  let countdownInstance = null;
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    // Se un countdown è già in corso, usa il metodo stopCountdown per fermarlo
+    if (countdownInstance) {
+      countdownInstance.stopCountdown();
+    }
+
+    // Seleziona i campi input all'interno del form con i nuovi ID
+    const giorno = parseInt(document.querySelector("#input-giorno").value);
+    const mese = parseInt(document.querySelector("#input-mese").value);
+    const anno = parseInt(document.querySelector("#input-anno").value);
+    const ore = parseInt(document.querySelector("#input-ore").value);
+    const minuti = parseInt(document.querySelector("#input-minuti").value);
+    const secondi = parseInt(document.querySelector("#input-secondi").value);
+
+    countdownInstance = new Countdown(giorno, mese, anno, ore, minuti, secondi);
+  });
+});
